04728433b6e701fb81a1a9922bfe97708f70e609
Aleksandr Zidyganov <alex@csscoder.pro>
2020-08-07 23:37:39 +0500

    fix typ

--- src/components/Employee/Advantage/Advantage.js
+++ src/components/Employee/Advantage/Advantage.js
@@ -14,21 +14,21 @@ const Members = () => {
               <img src={"/images/Employee/icon/i1.svg"} alt="img" width={109}/>
             </div>
             <div className="advantage-employee__item-title">No Need to Wait</div>
-            <div className="advantage-employee__item-note">Your earning are available to you in seconds!</div>
+            <div className="advantage-employee__item-note">Your earnings are available to you in seconds!</div>
           </div>
           <div className="advantage-employee__item">
             <div className="advantage-employee__item-ico">
               <img src={"/images/Employee/icon/i2.svg"} alt="img" width={86}/>
             </div>
             <div className="advantage-employee__item-title">Keep Track</div>
-            <div className="advantage-employee__item-note">Build your financial IQ & track your earning in real-time.</div>
+            <div className="advantage-employee__item-note">Build your financial IQ & track your earnings in real-time.</div>
           </div>
           <div className="advantage-employee__item">
             <div className="advantage-employee__item-ico">
               <img src={"/images/Employee/icon/i3.svg"} width={68} alt="img"/>
             </div>
             <div className="advantage-employee__item-title">Invest</div>
-            <div className="advantage-employee__item-note">Save & Grow your earning in real-time with us.</div>
+            <div className="advantage-employee__item-note">Save & Grow your earnings in real-time with us.</div>
           </div>
         </div>
       </div>
